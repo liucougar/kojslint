f4b392313b9bf93aa576c503648d48d3f8f5fd18
liucougar <liucougar@gmail.com>
2010-12-08 13:14:18 -0800

    no need to explicitly list false options

--- chrome/content/kojslint.js
+++ chrome/content/kojslint.js
@@ -47,72 +47,21 @@ window.extensions.KJSLINT = (function () {
         fileSavedobserver, // observer used to listen to file saved event
         globalPrefsSet, // where to find the preferences within komodo
         options = {
-            
-            adsafe : false, // if ADsafe should be enforced
-            
             bitwise : true, // if bitwise operators should not be allowed
-            
-            browser : false, // if the standard browser globals should be predefined
-            
-            cap : false, // if upper case HTML should be allowed
-            
-            css : false, // if CSS workarounds should be tolerated
-            
-            debug : false, // if debugger statements should be allowed
-            
-            devel : false, // if logging should be allowed (console, alert, etc.)
-            
             eqeqeq : true, // if === should be required
-            
-            es5 : false, // if ES5 syntax should be allowed
-            
-            evil : false, // if eval should be allowed
-            
-            forin : false, // if for in statements must filter
-            
-            fragment : false, // if HTML fragments should be allowed
-            
             immed : true, // if immediate invocations must be wrapped in parens
-            
             indent : 4,
-            
-            laxbreak : false, // if line breaks should not be checked
-            
             maxerr : 50,
-            
             maxlen : 250,
-            
             newcap : true, // if constructor names must be capitalized
-            
             nomen : true, // if names should be checked
-            
-            on : false, // if HTML event handlers should be allowed
-            
             onevar : true, // if only one var statement per function should be allowed
-            
-            passfail : false, // if the scan should stop on first error
-            
             plusplus : true, // if increment/decrement should not be allowed
-            
             predef : '', 
-            
             regexp : true, // if the . should not be allowed in regexp literals
-            
-            rhino : false, // if the Rhino environment globals should be predefined
-            
             undef : true, // if variables should be declared before used
-            
-            safe : false, // if use of some browser features should be restricted
-            
-            windows : false, // if MS Windows specific globals should be predefined
-            
             strict : true, // require the "use strict"; pragma
-            
-            sub : false, // if all forms of subscript notation are tolerated
-            
-            white : true, // if strict whitespace rules apply
-            
-            widget : false  // if the Yahoo Widgets globals should be predefined
+            white : true // if strict whitespace rules apply
         },
         prefsName = 'kJSLintPrefs', // name of stringPrefence in Komodo's prefs.xml
         prefsObject, // preferences
