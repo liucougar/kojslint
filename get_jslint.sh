465a835ad9ad4e640f7525e7eb29fed975e3edcb
Jak Spalding <jak.spalding@gmail.com>
2011-10-11 23:58:33 +0100

    Fix shell script
    Uses curl to fetch file from github

--- get_jslint.sh
+++ get_jslint.sh
@@ -1,40 +1,13 @@
 #!/bin/bash
-if [ ! -d chrome ]; then
+if [ -d kojslint ]; then
 	echo "automatically download jslint and modify it so it can be used in this extension; Please run this script under the root dir of kojslint."
 	exit 1
 fi
 
-cd modules
-JSLINT_URL='http://www.jslint.com/jslint.js'
-JSLINT_URL_SRC='https://github.com/douglascrockford/JSLint/raw/master/fulljslint.js'
-#wget --no-check-certificate https://github.com/douglascrockford/JSLint/raw/master/fulljslint.js
-wget -q $JSLINT_URL -O fulljslint.js
-echo '/*
-Copyright (c) 2002 Douglas Crockford (www.JSLint.com)
+cd chrome; cd content
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-The Software shall be used for Good, not Evil.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-var EXPORTED_SYMBOLS=["JSLINT"];' > jslint.js
-cat fulljslint.js | sed -e 's/"use strict";//;s/\r//g;s/^\/\/.*//;' | sed -e 'N;s/^\n//;' >> jslint.js
-rm -f fulljslint.js
-#echo "window.extensions.JSLINT=JSLINT;" >> jslint.js
+JSLINT_URL='https://raw.github.com/douglascrockford/JSLint/master/jslint.js'
+curl $JSLINT_URL -o jslint.js
+echo "window.extensions = window.extensions || {}; window.extensions.JSLINT=JSLINT;" >> jslint.js
 
 echo "successfully downloaded jslint"
