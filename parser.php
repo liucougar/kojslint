94acc4d577281ce3bc2903fbac8981673782efe5
Jak Spalding <jak.spalding@gmail.com>
2011-10-14 07:43:52 +0100

    Added description for parser

--- parser.php
+++ parser.php
@@ -1,5 +1,9 @@
 #!/usr/bin/php
 <?php
+
+/* This file converts the description of options in jslint.js to the format used
+   in the xul file */
+
 $contents = file_get_contents('php://stdin');
 $result = preg_replace("/\s+'?([a-z0-9]+)'?\s+([^\n]+)/um", "<checkbox id=\"$1\" class=\"kojslint_checkbox\" label=\"$2\" />", $contents);
 echo $result;
